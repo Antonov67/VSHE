f3141fd3c415c0e70dcc91a257ee21e893822942
Dm Ant <Dm@Ant.ru>
2023-01-19 09:47:47 +0300

    MainViewModel.java, HseRepository.java

---
+++ app/src/main/java/org/hse/timetable/HseRepository.java
@@ -0,0 +1,31 @@
+package org.hse.timetable;
+
+import android.content.Context;
+
+import androidx.lifecycle.LiveData;
+
+import java.util.Date;
+import java.util.List;
+
+public class HseRepository {
+    private DatabaseManager databaseManager;
+    private HseDao dao;
+
+    public HseRepository(Context context){
+        databaseManager = DatabaseManager.getInstance(context);
+        dao = databaseManager.getHseDao();
+    }
+
+    public LiveData<List<GroupEntity>> getGroups(){
+        return dao.getAllGroup();
+    }
+
+    public LiveData<List<TeacherEntity>> getTeachers(){
+        return dao.getAllTeacher();
+    }
+
+    public LiveData<List<TimeTableWithTeacherEntity>> getTimeTableTeacherByDate(Date date){
+        return dao.getTimetableTeacher();
+    }
+
+}
---
+++ app/src/main/java/org/hse/timetable/MainViewModel.java
@@ -0,0 +1,32 @@
+package org.hse.timetable;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.Date;
+import java.util.List;
+
+public class MainViewModel extends AndroidViewModel {
+
+    private HseRepository repository;
+
+    public MainViewModel(@NonNull Application application){
+        super(application);
+        repository = new HseRepository(application);
+    }
+
+    public LiveData<List<GroupEntity>> getGroups(){
+        return repository.getGroups();
+    }
+
+    public LiveData<List<TeacherEntity>> getTEachers(){
+        return repository.getTeachers();
+    }
+
+    public LiveData<List<TimeTableWithTeacherEntity>> getTimeTableTeacherByDate(Date date) {
+        return repository.getTimeTableTeacherByDate(date);
+    }
+}
